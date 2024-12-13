93d2997ce3f16e6f115f81d4273354f6d2fce477
thenextsdxnjs <88262029+thenextsdxnjs@users.noreply.github.com>
2024-12-13 17:06:12 +0545

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("sellScrap").addEventListener("click", () => {
+    alert("Sell Scrap feature is under development.");
+});
+
+document.getElementById("requestPickup").addEventListener("click", () => {
+    alert("Request Pickup feature is under development.");
+});
